59ab3dac4cceed0e72edd66bb32cc98971f6a49c
Steven barnett <steve@sabarnett.co.uk>
2025-12-12 15:18:14 +0000

    Initial game commit

---
+++ BlockSmash/GameScene.swift
@@ -0,0 +1,265 @@
+//
+//  GameScene.swift
+//  BlockSmash
+//
+//  Created by Paul Hudson on 16/10/2017.
+//  Copyright © 2017 Paul Hudson. All rights reserved.
+//
+
+import SpriteKit
+
+@objcMembers
+class GameScene: SKScene {
+    let scoreLabel = SKLabelNode(fontNamed: "Noteworthy-Bold")
+    let timer = SKShapeNode(rect: CGRect(x: 0, y: 0, width: 500, height: 20))
+    let music = SKAudioNode(fileNamed: "winner-winner")
+
+    let itemSize: CGFloat = 50
+    let itemsPerColumn = 12
+    let itemsPerRow = 18
+    let itemImages = ["black", "blue", "green", "purple", "red", "yellow", "orange"]
+
+    var cols = [[Item]]()
+    var currentMatches = Set<Item>()
+    var gameStartTime: TimeInterval?
+    var isGameOver = false
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "SCORE: \(score)"
+        }
+    }
+
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "blocks")
+        background.zPosition = -2
+        addChild(background)
+
+        for x in 0 ..< itemsPerRow {
+            var col = [Item]()
+
+            for y in 0 ..< itemsPerColumn {
+                let item = createItem(row: y, col: x)
+                col.append(item)
+            }
+
+            cols.append(col)
+        }
+
+        scoreLabel.horizontalAlignmentMode = .right
+        scoreLabel.position = CGPoint(x: frame.maxX - 80, y: frame.maxY - 80)
+        addChild(scoreLabel)
+
+        score = 0
+
+        timer.fillColor = .green
+        timer.strokeColor = .clear
+        timer.position = CGPoint(x: frame.minX + 70, y: frame.maxY - 80)
+        addChild(timer)
+
+        addChild(music)
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+
+        if isGameOver {
+            // Play again pressed?
+            if let _ = nodes(at: location).first(where: {$0.name == "playAgain"}) {
+                if let scene = SKScene(fileNamed: "GameScene") {
+                    scene.scaleMode = .aspectFill
+                    self.view?.presentScene(scene)
+                }
+            }
+            return
+        }
+
+        guard let tappedItem = item(at: location) else { return }
+        isUserInteractionEnabled = false
+        currentMatches.removeAll()
+        run(SKAction.playSoundFileNamed("pop", waitForCompletion: false))
+
+        if tappedItem.name == "bomb" {
+            triggerSpecialItem(tappedItem)
+        }
+
+        match(item: tappedItem)
+        removeMatches()
+        moveDown()
+
+        adjustScore()
+    }
+
+    override func update(_ currentTime: TimeInterval) {
+        if let gameStartTime = gameStartTime {
+            let elapsed = currentTime - gameStartTime
+//            let remaining = 3 - elapsed
+            let remaining = 100 - elapsed
+            timer.xScale = max(0, CGFloat(remaining) / 100)
+            if remaining < 25 {
+                timer.fillColor = .red
+            } else if remaining < 60 {
+                timer.fillColor = .orange
+            }
+
+            if remaining <= 0 {
+                endGame()
+            }
+        } else {
+            gameStartTime = currentTime
+        }
+    }
+
+    func position(for item: Item) -> CGPoint {
+        let xOffset: CGFloat = -430
+        let yOffset: CGFloat = -300
+
+        let x = xOffset + itemSize * CGFloat(item.col)
+        let y = yOffset + itemSize * CGFloat(item.row)
+        return CGPoint(x: x, y: y)
+    }
+
+    func createItem(row: Int, col: Int, startOffScreen: Bool = false) -> Item {
+        let itemImage: String
+
+        if startOffScreen && Int.random(in: 0..<24) == 0 {
+            itemImage = "bomb"
+        } else {
+            itemImage = itemImages.randomElement()!
+        }
+
+        let item = Item(imageNamed: itemImage)
+        item.name = itemImage
+        item.row = row
+        item.col = col
+
+        if startOffScreen {
+            // 1: Calculate the position
+            let finalPosition = position(for: item)
+
+            // move it higher
+            item.position = finalPosition
+            item.position.y += 600
+
+            // create an animation to move it to the final position
+            let action = SKAction.move(to: finalPosition, duration: 0.5)
+
+            // run the animation then re-enable user interaction when it finishes
+            item.run(action) {
+                self.isUserInteractionEnabled = true
+            }
+        } else {
+            item.position = position(for: item)
+        }
+
+        addChild(item)
+        return item
+    }
+
+    func item(at point: CGPoint) -> Item? {
+        let items = nodes(at: point).compactMap { $0 as? Item }
+        return items.first
+    }
+
+    func match(item original: Item) {
+        var checkItems = [Item?]()
+
+        currentMatches.insert(original)
+        let pos = original.position
+
+        checkItems.append(item(at: CGPoint(x: pos.x, y: pos.y - itemSize)))
+        checkItems.append(item(at: CGPoint(x: pos.x, y: pos.y + itemSize)))
+        checkItems.append(item(at: CGPoint(x: pos.x - itemSize, y: pos.y)))
+        checkItems.append(item(at: CGPoint(x: pos.x + itemSize, y: pos.y)))
+
+        for case let check? in checkItems {
+            if currentMatches.contains(check) { continue }
+
+            if check.name == original.name || original.name == "bomb" {
+                match(item: check)
+            }
+        }
+    }
+
+    func removeMatches() {
+        let sortedMatches = currentMatches.sorted {
+            $0.row > $1.row
+        }
+
+        for item in sortedMatches {
+            cols[item.col].remove(at: item.row)
+            item.removeFromParent()
+        }
+    }
+
+    func moveDown() {
+        // move down any items that need it
+        for (columnIndex, col) in cols.enumerated() {
+            for (rowIndex, item) in col.enumerated() {
+                item.row = rowIndex
+
+                let action = SKAction.move(to: position(for: item), duration: 0.1)
+                item.run(action)
+            }
+
+            while cols[columnIndex].count < itemsPerColumn {
+                let item = createItem(row: cols[columnIndex].count, col: columnIndex, startOffScreen: true)
+                cols[columnIndex].append(item)
+            }
+        }
+    }
+
+    func triggerSpecialItem(_ item: Item) {
+        let flash = SKSpriteNode(color: .white, size: frame.size)
+        flash.zPosition = -1
+        addChild(flash)
+
+        run(SKAction.playSoundFileNamed("smart-bomb", waitForCompletion: false))
+
+        flash.run(SKAction.fadeOut(withDuration: 0.3)) {
+            flash.removeFromParent()
+        }
+    }
+
+    func penalizePlayer() {
+        for col in cols {
+            for item in col {
+                let changeTo = itemImages.randomElement()!
+                item.name = changeTo
+                item.texture = SKTexture(imageNamed: changeTo)
+            }
+        }
+    }
+
+    func adjustScore() {
+        let newScore = currentMatches.count
+
+        if newScore == 1 {
+            penalizePlayer()
+        } else if newScore == 2 {
+            // Do nothing
+        } else {
+            let matchCount = min(newScore, 16)
+            let scoreToAdd = pow(2, Double(matchCount))
+            score += Int(scoreToAdd)
+        }
+    }
+
+    func endGame() {
+        guard isGameOver == false else { return }
+        isGameOver = true
+
+        let gameOver = SKSpriteNode(imageNamed: "game-over")
+        gameOver.zPosition = 100
+        addChild(gameOver)
+
+        let playAgain = SKLabelNode(fontNamed: "Noteworthy-Bold")
+        playAgain.text = "Tap to play again"
+        playAgain.name = "playAgain"
+        playAgain.zPosition = 100
+        playAgain.position.y = gameOver.position.y - 190
+        addChild(playAgain)
+
+        music.removeFromParent()
+    }
+}
---
+++ BlockSmash/Item.swift
@@ -0,0 +1,18 @@
+//
+// -----------------------------------------
+// Original project: BlockSmash
+// Original package: BlockSmash
+// Created on: 05/11/2025 by: Steven Barnett
+// Web: http://www.sabarnett.co.uk
+// GitHub: https://www.github.com/sabarnett
+// -----------------------------------------
+// Copyright © 2025 Steven Barnett. All rights reserved.
+//
+
+import SpriteKit
+
+class Item: SKSpriteNode {
+    var row = -1
+    var col = -1
+    
+}
